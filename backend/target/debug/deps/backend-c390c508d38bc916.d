8845cbf525d1939e839ddc2598e7fd0912bad772
cosmas-ships <cosmascodes@gmail.com>
2025-10-29 14:28:27 +0300

    Fix .gitignore and remove tracked build artifacts

--- backend/target/debug/deps/backend-c390c508d38bc916.d
+++
@@ -1,5 +0,0 @@
-/home/cosmas/Documents/Github/rental_management_system/backend/target/debug/deps/backend-c390c508d38bc916.d: src/main.rs
-
-/home/cosmas/Documents/Github/rental_management_system/backend/target/debug/deps/libbackend-c390c508d38bc916.rmeta: src/main.rs
-
-src/main.rs:
