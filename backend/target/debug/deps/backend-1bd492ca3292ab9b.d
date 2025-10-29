6bc0504c43e079644d263befcde2a8e579f6c6f0
cosmas-ships <cosmascodes@gmail.com>
2025-10-29 14:22:31 +0300

    Lost all files today

---
+++ backend/target/debug/deps/backend-1bd492ca3292ab9b.d
@@ -0,0 +1,5 @@
+/home/cosmas/Documents/Github/rental_management_system/backend/target/debug/deps/backend-1bd492ca3292ab9b.d: src/main.rs
+
+/home/cosmas/Documents/Github/rental_management_system/backend/target/debug/deps/libbackend-1bd492ca3292ab9b.rmeta: src/main.rs
+
+src/main.rs:
